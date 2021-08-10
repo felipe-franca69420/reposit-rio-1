e02e74877e1c8739e59edb861fec35b6f877f90c
Felipe <fjonckfranca@gmail.com>
2021-08-10 09:13:45 -0300

    editado

--- FundamentosTI/exemplos/avaliacao_pratica/usuarios_nao_humanos.sh
+++ FundamentosTI/exemplos/avaliacao_pratica/usuarios_nao_humanos.sh
@@ -4,4 +4,15 @@
 #!/bin/bash
 clear
 
-cut -d: -f1 /etc/passwd
+valor=$(cut -d":" -f3 /etc/passwd)
+for i in $valor
+do
+        if [[ $i -gt 0 ]]
+        then
+                if [[ $i -lt 1000 ]]
+                then
+                        cut -s -d: -f1 /etc/passwd > texto.txt
+                        sed -n $i"p" texto.txt
+                fi
+        fi
+done
